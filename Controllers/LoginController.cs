9991a65278847200d1b89e228a0baea3820810f7
Henry Craig <h.craig52@yahoo.com>
2019-06-27 15:54:40 -0400

    working on the login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace InstaclonePhotogram.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult index()
+        {
+            return View();
+        }
+
+        //Alright, so I think initially I can just have it check the return of the username box initially
+        //And if it matches in the database then I can return something saying it worked!
+        //Like I could totally just do that in the console
+
+        //Afterwards I can add the password but that should be easy
+
+
+    }
+}
